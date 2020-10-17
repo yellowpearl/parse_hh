f25b07c80664f4454b461bd654c1cd4b2982b863
yellowpearl <yellowpearl@yandex.ru>
2020-10-17 15:38:49 +0300

    end before refactor

--- mpw.py
+++ mpw.py
@@ -1,28 +1,31 @@
-import collections
 import os
-dir_name = 'backup_cpo/cpo'
-c = collections.Counter()
-
+import string
+import nltk
+from nltk import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+dir_name = 'pm'
+spec_chars = string.punctuation + '\n\xa0«»\t—…' + string.digits[:2] + string.digits[3:]
 cwd = os.getcwd()
-print(cwd)
 
 full_path_dir = cwd+f'/{dir_name}'
 os.chdir(full_path_dir)
 
 files = os.listdir(full_path_dir)
-print(files)
 for filetxt in files:
-    print(filetxt)
     file = open(f'{filetxt}', 'r')
     text = file.read()
-    text_list = text.split(' ')
-
-    for word in text_list:
-        c[word] += 1
-
-    result = '\n'.join([f'{key.capitalize()}: {value}' for key, value in c.most_common() if len(key)>2])
-
-    file_n = open(f'ANS_{filetxt[:-4]}.txt', 'w')
+    text = text.lower()
+    text = "".join([ch for ch in text if ch not in spec_chars])
+    len_text = len(text)
+    text_tokens = word_tokenize(text)
+    text = nltk.Text(text_tokens)
+    russian_stopwords = stopwords.words("russian")
+    filtered_sentence = [w for w in text_tokens if not w in russian_stopwords]
+    fdist = FreqDist(filtered_sentence)
+    fdist = fdist.most_common(150)
+    result = '\n'.join([f'{key.capitalize()}: {value}' for key, value in fdist if len(key) > 2])
+    file_n = open(f'ANS_{filetxt}.txt', 'w')
     file_n.write(result)
     file_n.close()
     file.close()
